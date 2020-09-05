ce843848b0b7faf416c6d7d3b504a8c4c6a2d715
Tanner <thebow25@gmail.com>
2020-09-04 23:07:03 -0600

    Camera to follow player

---
+++ Gatekeeper/Assets/Scripts/Camera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Camera : MonoBehaviour
+{
+
+	public Transform playerTransform;
+
+	private Vector3 _cameraOffset;
+
+	public float SmoothFactor = 0.5f;
+	
+// Use this for initialization
+	void Start ()
+	{
+		_cameraOffset = transform.position - playerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 newPos = playerTransform.position + _cameraOffset;
+
+		transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+	}
+}
