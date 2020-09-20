49b3b0c7c5f6ad532a9c6442b8ed885445e2f9e3
Tanner <thebow25@gmail.com>
2020-09-19 22:48:23 -0600

    Gem collectiong print

---
+++ Gatekeeper/Assets/Scripts/WinScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WinScript : MonoBehaviour
+{
+
+	public UnityEvent script;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+		print("Gem Collected Move Forward");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
