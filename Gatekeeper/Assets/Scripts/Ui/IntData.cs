2507764f6faec22473e349390b08c71901459c7c
Tanner <thebow25@gmail.com>
2020-10-03 22:11:55 -0600

    Added uitextcontroller score script with int data to it.

---
+++ Gatekeeper/Assets/Scripts/Ui/IntData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+	 
+	public int gemScore = 0;
+	// Use this for initialization
+	void Reset ()
+	{
+		gemScore = 0;
+	}
+
+	public void UpdategemScore(int number)
+	{
+		gemScore += number;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Gatekeeper/Assets/Scripts/Ui/UiTextEd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
+public class UiTextEd : MonoBehaviour
+{
+
+
+	private Text Label;
+
+	public void UpdateText(IntData data)
+	{
+		Label.text = data.gemScore.ToString();
+	}
+
+	private void Awake()
+	{
+		Label = GetComponent<Text>();
+	}
+}
