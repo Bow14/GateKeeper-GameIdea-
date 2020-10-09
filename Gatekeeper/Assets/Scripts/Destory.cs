9d994a8360a07c925203e797be70a78ea3ac3502
Tanner <thebow25@gmail.com>
2020-10-08 23:28:39 -0600

    Testing destory object in my project.

---
+++ Gatekeeper/Assets/Scripts/Destory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destory : MonoBehaviour
+{
+
+
+	public float delay = 4f;
+	// Use this for initialization
+	void Awake () {
+		Destroy(gameObject, delay);
+	}
+
+	public void DestroyOnImpact()
+	{
+		Destroy((gameObject));
+	}
+	// Update is called once per frame
+	
+}
