216868352f6a477a16a84b5f56bec984be0b9566
Tanner <thebow25@gmail.com>
2020-10-12 22:39:17 -0600

    Test destroy scripts again they work well just gotta fix collision

--- Gatekeeper/Assets/Scripts/Destory.cs
+++ Gatekeeper/Assets/Scripts/Destory.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Destory : MonoBehaviour
 {
-
-
-	public float delay = 4f;
-	// Use this for initialization
-	void Awake () {
-		Destroy(gameObject, delay);
-	}
-
-	public void DestroyOnImpact()
+	public void OnCollisionEnter(Collision collision)
 	{
-		Destroy((gameObject));
+		if (collision.gameObject.tag == "Player")
+		{
+			Destroy(collision.gameObject);
+			Debug.Log("Ruined");
+		}
 	}
+
+//	public float delay = 4f;
+//	// Use this for initialization
+//	void Awake () {
+//		Destroy(gameObject, delay);
+//	}
+
+//	public void DestroyOnImpact()
+//	{
+//		Destroy((gameObject));
+//	}
 	// Update is called once per frame
 	
 }
---
+++ Gatekeeper/Assets/Scripts/destroy2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy2 : MonoBehaviour {
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("GameController"))
+		{
+			Destroy(collision.gameObject);
+			Debug.Log("Gammer");
+			
+		}
+
+		
+	}
+}
