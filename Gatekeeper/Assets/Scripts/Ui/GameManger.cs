5558b091f27ff2feb0c0c47a8c2cc46892d6414a
Tanner <thebow25@gmail.com>
2020-10-27 22:10:01 -0600

    Started level end scene and such

---
+++ Gatekeeper/Assets/Scripts/Ui/GameManger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManger : MonoBehaviour
+{
+	//This is from brackeys tutorial video
+	private bool gameHasEnded = false;
+
+	public void CompleteLevel()
+	{
+		Debug.Log("Level Won!");
+	}
+
+	public float restartDeylay = 1f;
+
+	public void EndGame()
+	{
+		if (gameHasEnded == false)
+		{
+			gameHasEnded = true;
+			Debug.Log("Game Over");
+			Invoke("Restart", restartDeylay);
+		}
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ Gatekeeper/Assets/Scripts/Ui/LevelEnd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+
+	public GameManger gameManger;
+	private void OnTriggerEnter()
+	{
+		gameManger.CompleteLevel();
+	}
+}
