eab2ec1953097f45cebf907efef310792e651710
Tanner <thebow25@gmail.com>
2021-03-12 19:57:12 -0700

    Springbreak and midterms over now making keys aka gems to unlock doors

---
+++ Gatekeeper/Assets/Gemholder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gemholder : MonoBehaviour
+    //tutorial on how to use doors with keys aka my gems
+    //https://www.youtube.com/watch?v=MIt0PJHMN5Y&t=0s credit goes to Code monkey with this tutorial
+{
+    private List<Key.KeyType> keyList;
+
+    private void Awake()
+    {
+        keyList = new List<Key.KeyType>();
+    }
+}
+
+internal class Key
+{
+    public class KeyType
+    {
+        
+    }
+}
